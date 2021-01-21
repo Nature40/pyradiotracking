443450b346e5c37756ebcc5278f6ef5a11588f22
Jonas Höchst <git@jonashoechst.de>
2021-01-22 00:26:29 +0100

    updated rpi temperature monitor

--- etc/rpi-overclocking-monitor.sh
+++ etc/rpi-overclocking-monitor.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 while true; do
-	echo $(date) - $(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq) Hz - $(/opt/vc/bin/vcgencmd measure_temp)
+	temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+
+	echo $(date) - $(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq) Hz - ${temp:0:2}.${temp:2} °C
 	sleep 1
-done	
+done
